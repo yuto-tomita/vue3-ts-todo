c09b6d5a88b80afdf464df0760f977c8f049e56a
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-07-23 17:19:41 +0900

    Cardコンポーネントの作成

--- src/components/ui/Card/CardUi.vue
+++ src/components/ui/Card/CardUi.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 </script>
 
-<!-- <template>
-
-</template> -->
+<template>
+  <div class="h-60 w-80 border border-solid border-gray-800">
+    <div>
+      <slot name="title" />
+    </div>
+  </div>
+</template>
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Input } from './Input'
 export { default as Button } from './Button'
 export { default as Datepick } from './Input/DatepickUi.vue'
+export { default as Card } from './Card'
