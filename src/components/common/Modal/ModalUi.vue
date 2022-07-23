d451f7e15e1cf47848a75b1500fc99a8b159a9b5
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-07-23 17:04:49 +0900

    Modalのスタイル修正

--- src/components/common/Modal/ModalUi.vue
+++ src/components/common/Modal/ModalUi.vue
@@ -2,8 +2,8 @@
   <!-- Vue3から追加された、teleportを使用することで指定したセレクタの直下にDOM生成することができる。 -->
   <teleport to='#app'>
     <div class="fixed top-0 left-0 z-10 box-border flex h-full w-screen bg-black/20 align-middle">
-      <div class="m-auto h-3/6 w-3/6 rounded-lg bg-white">
-        <div class="my-3 ml-3 text-2xl">
+      <div class="m-auto h-4/6 w-5/6 rounded-lg bg-white">
+        <div class="m-3">
           <slot />
         </div>
       </div>
