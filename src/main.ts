4847075a76d169669eb0d180586d66b1a475f292
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-07-26 15:29:24 +0900

    createApp関連の解説文の追加

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import './assets/index.css'
 import router from './modules/router'
 import { createPinia } from 'pinia'
 
+// アプリケーションインスタンスの作成
+// グローバルに使いたいコンポーネントやプラグインを登録できる
+
+// createApp(rootComponent)を使用することで、rootComponentをレンダリングの起点としてアプリケーションを作成していくことができる
+// /index.htmlの`#app`の配下にVueがDOMをマウントさせる
+
+// createAppでApp.vueを起点としたDOMが生成され、mount('#app')で登録しているルートコンポーネントのインスタンスを返す(画面にアプリケーションが表示される　)
 createApp(App)
   .use(router)
   .use(createPinia())
