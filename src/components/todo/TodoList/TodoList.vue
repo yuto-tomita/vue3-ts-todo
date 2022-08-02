9c63487cf526a4b40bb854c7b76a7e49e97158c8
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-08-02 22:19:29 +0900

    Todoを表示するためのコンポーネントを作成

---
+++ src/components/todo/TodoList/TodoList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { Todo } from '@/components/todo/types'
+
+interface TodoListProps {
+	todoList: Todo[]
+}
+defineProps<TodoListProps>()
+</script>
+
+<template>
+  <!-- "in"または"of"でもv-forディレクティブを使用できる -->
+  <!-- 左辺の"todo"は配列の要素、"index"は配列の添字, 右辺は表示させる配列のソースデータ -->
+  <div
+    v-for="(todo, index) in todoList"
+    :key="index"
+  >
+    {{ todo.todo }}
+  </div>
+</template>
--- src/components/todo/index.ts
+++ src/components/todo/index.ts
@@ -1,1 +1,2 @@
 export { default as DetailSettingModal } from './DetailSetting'
+export { default as TodoList } from './TodoList'
