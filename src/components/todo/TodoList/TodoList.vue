deae6ca0f4adc38ffe5afc886dae2b0792f0170b
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-08-04 13:16:04 +0900

    TODO一覧を表示するためのコンポーネントの作成

--- src/components/todo/TodoList/TodoList.vue
+++ src/components/todo/TodoList/TodoList.vue
@@ -1,20 +1,75 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
-import type { Todo } from '@/components/todo/types'
+import { defineProps, defineEmits, computed } from 'vue'
+import { Todo, Status } from '@/components/todo/types'
+import { CheckIcon, XIcon } from '@heroicons/vue/solid'
 
 interface TodoListProps {
 	todoList: Todo[]
 }
-defineProps<TodoListProps>()
+const props = defineProps<TodoListProps>()
+
+// emitsの型指定
+// onDeleteイベントを発火するにはnumber型のidプロパティが必要になる
+const emits = defineEmits<{(e: 'onDelete', id: number): void, (e: 'onComplete', id: number): void
+}>()
+
+const findTodo = (id: number) => {
+  return props.todoList.find(val => val.id === id)
+}
+
+/** TodoのStatusがCompleteならば、チェックのスタイルを返す */
+const checkBackground = computed(() => {
+  return (id: number) => {
+    return findTodo(id)?.status === Status.COMPLETE ? 'bg-slate-900' : ''
+  }
+})
+
+/** TODOのStatusがCompleteならば、斜線を引く */
+const checkLine = computed(() => {
+  return (id: number) => {
+    return findTodo(id)?.status === Status.COMPLETE ? 'line-through' : ''
+  }
+}) 
+
+const onClickDeleteIcon = (id: number) => {
+  // error!
+  // emits('onDelete')
+
+  // emitすることで親コンポーネント側にイベントを発火させることができる
+  emits('onDelete', id)
+}
+
+const onClickCompleteIcon = (id: number) => {
+  emits('onComplete', id)
+}
 </script>
 
 <template>
-  <!-- "in"または"of"でもv-forディレクティブを使用できる -->
-  <!-- 左辺の"todo"は配列の要素、"index"は配列の添字, 右辺は表示させる配列のソースデータ -->
-  <div
-    v-for="(todo, index) in todoList"
-    :key="index"
-  >
-    {{ todo.todo }}
+  <div class="m-auto mt-12 w-4/6">
+    <!-- "in"または"of"でもv-forディレクティブを使用できる -->
+    <!-- 左辺の"todo"は配列の要素、"index"は配列の添字, 右辺は表示させる配列のソースデータ -->
+    <div
+      v-for="(todo) in todoList"
+      :key="todo.id"
+      class="relative m-auto mb-4 border border-slate-400 py-4 px-20"
+    >
+      <!-- v-bindとclassを組み合わせることにより、動的にclassを切り替えることができる -->
+      <div
+        :class="`absolute left-5 top-7 h-7 w-7 cursor-pointer rounded-full border border-slate-500 ${checkBackground(todo.id)}`"
+        @click="onClickCompleteIcon(todo.id)"
+      >
+        <CheckIcon class="m-auto h-full w-5 text-white" />
+      </div>
+      <p :class="`select-none text-base overflow-x-auto py-3 ${checkLine(todo.id)}`">
+        {{ todo.todo }}
+      </p>
+
+      <XIcon
+        class="absolute right-4 top-7 h-6 cursor-pointer"
+        @click="onClickDeleteIcon(todo.id)"
+      />
+      <!-- error! 引数にidを渡していないから -->
+      <!-- @click="onClickDeleteIcon" -->
+    </div>
   </div>
 </template>
